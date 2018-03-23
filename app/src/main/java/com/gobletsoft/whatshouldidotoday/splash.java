1ad2299198b0da8be4482f2fd0b8852587a49a15
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2018-03-23 17:18:30 +0300

    core files added.

---
+++ app/src/main/java/com/gobletsoft/whatshouldidotoday/splash.java
@@ -0,0 +1,48 @@
+package com.gobletsoft.whatshouldidotoday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+
+                try{
+
+                    sleep(1000);
+                }catch(InterruptedException e){
+
+                    e.printStackTrace();
+                }finally{
+
+                    Intent intent = new Intent(splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause(){
+
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
